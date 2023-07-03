53d51e36c7c2fa02ea1888749a6c3f7839dbb759
Siddharth Hota <siddharthhota26@gmail.com>
2023-07-04 00:20:55 +0530

    id

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
     }
 
     return (
-        <div>
+        <div className="Calculator">
             <input type="text" disabled value={input}></input>
             <div>
                 <button onClick={(e)=>{handleClick(e)}}>C</button>
@@ -26,24 +26,24 @@ const App = () => {
                 <button onClick={(e)=>{handleClick(e)}}>-</button>
             </div>
             <div>
-                <button onClick={(e)=>{handleClick(e)}}>7</button>
-                <button onClick={(e)=>{handleClick(e)}}>8</button>
-                <button onClick={(e)=>{handleClick(e)}}>9</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-7">7</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-8">8</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-9">9</button>
                 <button onClick={(e)=>{handleClick(e)}}>+</button>
             </div>
             <div>
-                <button onClick={(e)=>{handleClick(e)}}>4</button>
-                <button onClick={(e)=>{handleClick(e)}}>5</button>
-                <button onClick={(e)=>{handleClick(e)}}>6</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-4">4</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-5">5</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-6">6</button>
             </div>
             <div>
-                <button onClick={(e)=>{handleClick(e)}}>1</button>
-                <button onClick={(e)=>{handleClick(e)}}>2</button>
-                <button onClick={(e)=>{handleClick(e)}}>3</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-1">1</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-2">2</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-3">3</button>
                 <button onClick={(e)=>{handleClick(e)}}>=</button>
             </div>
             <div>
-                <button onClick={(e)=>{handleClick(e)}}>0</button>
+                <button onClick={(e)=>{handleClick(e)}} id="btn-0">0</button>
                 <button onClick={(e)=>{handleClick(e)}}>.</button>
                 <button></button>
             </div>
